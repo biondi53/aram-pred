15fd7a49262fdf9c5478641f40833c2fae43396a
biondi53 <biondi53@gmail.com>
2025-09-27 21:23:43 -0300

    Add files via upload

---
+++ items.js
@@ -0,0 +1,438 @@
+[
+  {
+    "name": "Warp Stream",
+    "image": "https://omeda.city/images/items/Quantum-Core.webp"
+  },
+  {
+    "name": "Dawnstar",
+    "image": "https://omeda.city/images/items/Dawnstar.webp"
+  },
+  {
+    "name": "Crescelia",
+    "image": "https://omeda.city/images/items/Crescelia.webp"
+  },
+  {
+    "name": "Dynamo",
+    "image": "https://omeda.city/images/items/Dynamo.webp"
+  },
+  {
+    "name": "Void Conduit",
+    "image": "https://omeda.city/images/items/VoidConduit.webp"
+  },
+  {
+    "name": "Windcaller",
+    "image": "https://omeda.city/images/items/Windcaller.webp"
+  },
+  {
+    "name": "Hexbound Bracers",
+    "image": "https://omeda.city/images/items/Hexbound-Bracers.webp"
+  },
+  {
+    "name": "Crystal Tear",
+    "image": "https://omeda.city/images/items/Crystal-Tear.webp"
+  },
+  {
+    "name": "Tainted Charm",
+    "image": "https://omeda.city/images/items/TaintedCharm.webp"
+  },
+  {
+    "name": "Lunaria",
+    "image": "https://omeda.city/images/items/Lunaria.webp"
+  },
+  {
+    "name": "Frosted Lure",
+    "image": "https://omeda.city/images/items/Frosted-Lure.webp"
+  },
+  {
+    "name": "Tainted Totem",
+    "image": "https://omeda.city/images/items/Flourishing-Totem.webp"
+  },
+  {
+    "name": "Marshal",
+    "image": "https://omeda.city/images/items/Marshal.webp"
+  },
+  {
+    "name": "Galaxy Greaves",
+    "image": "https://omeda.city/images/items/Galaxy-Greaves.webp"
+  },
+  {
+    "name": "Everbloom",
+    "image": "https://omeda.city/images/items/Everbloom.webp"
+  },
+  {
+    "name": "Vanguardian",
+    "image": "https://omeda.city/images/items/Star-Slayer.webp"
+  },
+  {
+    "name": "Truesilver Bracelet",
+    "image": "https://omeda.city/images/items/Truesilver-Bracelet.webp"
+  },
+  {
+    "name": "Spectra",
+    "image": "https://omeda.city/images/items/Spectra.webp"
+  },
+  {
+    "name": "Nuclear Rounds",
+    "image": "https://omeda.city/images/items/Nuclear-Rounds.webp"
+  },
+  {
+    "name": "Onixian Quiver",
+    "image": "https://omeda.city/images/items/Onixian-Quiver.webp"
+  },
+  {
+    "name": "Unbroken Will",
+    "image": "https://omeda.city/images/items/Unbroken-WIll.webp"
+  },
+  {
+    "name": "Flux Matrix",
+    "image": "https://omeda.city/images/items/Flux-Matrix.webp"
+  },
+  {
+    "name": "Dreambinder",
+    "image": "https://omeda.city/images/items/Dreambinder.webp"
+  },
+  {
+    "name": "Syonic Echo",
+    "image": "https://omeda.city/images/items/Syonic-Echo.webp"
+  },
+  {
+    "name": "Cybernetic Drive",
+    "image": "https://omeda.city/images/items/Cybernetic-Drive.webp"
+  },
+  {
+    "name": "Catalytic Drive",
+    "image": "https://omeda.city/images/items/Catalytic-Drive.webp"
+  },
+  {
+    "name": "Tainted Trident",
+    "image": "https://omeda.city/images/items/Tainted-Trident.webp"
+  },
+  {
+    "name": "Noxia",
+    "image": "https://omeda.city/images/items/Noxia.webp"
+  },
+  {
+    "name": "Gaia Greaves",
+    "image": "https://omeda.city/images/items/Gaia-Greaves.webp"
+  },
+  {
+    "name": "Echelon Cloak",
+    "image": "https://omeda.city/images/items/Echelon-Cloak.webp"
+  },
+  {
+    "name": "Void Helm",
+    "image": "https://omeda.city/images/items/Void-Helm.webp"
+  },
+  {
+    "name": "Overlord",
+    "image": "https://omeda.city/images/items/Nullifier.webp"
+  },
+  {
+    "name": "Nightfall",
+    "image": "https://omeda.city/images/items/Nightfall.webp"
+  },
+  {
+    "name": "Mindrazor",
+    "image": "https://omeda.city/images/items/Mindrazor.webp"
+  },
+  {
+    "name": "Dread",
+    "image": "https://omeda.city/images/items/Insanity.webp"
+  },
+  {
+    "name": "Fire Blossom",
+    "image": "https://omeda.city/images/items/Fire-Blossom.webp"
+  },
+  {
+    "name": "Mesmer",
+    "image": "https://omeda.city/images/items/Dreadwind.webp"
+  },
+  {
+    "name": "Caustica",
+    "image": "https://omeda.city/images/items/Caustica.webp"
+  },
+  {
+    "name": "Malady",
+    "image": "https://omeda.city/images/items/Blackout.webp"
+  },
+  {
+    "name": "Spear Of Desolation",
+    "image": "https://omeda.city/images/items/Spear-Of-Desolation.webp"
+  },
+  {
+    "name": "Astral Catalyst",
+    "image": "https://omeda.city/images/items/Astral-Catalyst.webp"
+  },
+  {
+    "name": "Tyranny",
+    "image": "https://omeda.city/images/items/Tyranny.webp"
+  },
+  {
+    "name": "Entropy",
+    "image": "https://omeda.city/images/items/Entropy.webp"
+  },
+  {
+    "name": "Plasma Blade",
+    "image": "https://omeda.city/images/items/Plasma-Blade.webp"
+  },
+  {
+    "name": "Penumbra",
+    "image": "https://omeda.city/images/items/Penumbra.webp"
+  },
+  {
+    "name": "Mistmeadow Buckler",
+    "image": "https://omeda.city/images/items/Mistmeadow-Buckler.webp"
+  },
+  {
+    "name": "Fist Of Razuul",
+    "image": "https://omeda.city/images/items/FistOfJazuul.webp"
+  },
+  {
+    "name": "Equinox",
+    "image": "https://omeda.city/images/items/Equinox.webp"
+  },
+  {
+    "name": "Earthshaker",
+    "image": "https://omeda.city/images/items/Earthshaker.webp"
+  },
+  {
+    "name": "Spirit Of Amir",
+    "image": "https://omeda.city/images/items/Spirit-Of-Amir.webp"
+  },
+  {
+    "name": "Orb Of Growth",
+    "image": "https://omeda.city/images/items/Orb-Of-Growth.webp"
+  },
+  {
+    "name": "Orb Of Enlightenment",
+    "image": "https://omeda.city/images/items/Orb-Of-Enlightenment.webp"
+  },
+  {
+    "name": "Giant's Ring",
+    "image": "https://omeda.city/images/items/Giants-Ring.webp"
+  },
+  {
+    "name": "Golem's Gift",
+    "image": "https://omeda.city/images/items/Zephyr.webp"
+  },
+  {
+    "name": "Timewarp",
+    "image": "https://omeda.city/images/items/Timewarp.webp"
+  },
+  {
+    "name": "Tainted Scepter",
+    "image": "https://omeda.city/images/items/Tainted-Scepter.webp"
+  },
+  {
+    "name": "Tainted Rounds",
+    "image": "https://omeda.city/images/items/Tainted-Rounds.webp"
+  },
+  {
+    "name": "Tainted Guard",
+    "image": "https://omeda.city/images/items/Palisade.webp"
+  },
+  {
+    "name": "Stonewall",
+    "image": "https://omeda.city/images/items/Stonewall.webp"
+  },
+  {
+    "name": "Spellbreaker",
+    "image": "https://omeda.city/images/items/Spellbreaker.webp"
+  },
+  {
+    "name": "Salvation",
+    "image": "https://omeda.city/images/items/Salvation.webp"
+  },
+  {
+    "name": "Prophecy",
+    "image": "https://omeda.city/images/items/Prophecy.webp"
+  },
+  {
+    "name": "Painweaver",
+    "image": "https://omeda.city/images/items/Painweaver.webp"
+  },
+  {
+    "name": "Omen",
+    "image": "https://omeda.city/images/items/Omen.webp"
+  },
+  {
+    "name": "Lifebinder",
+    "image": "https://omeda.city/images/items/Lifebinder.webp"
+  },
+  {
+    "name": "Tainted Bastion",
+    "image": "https://omeda.city/images/items/Leviathan.webp"
+  },
+  {
+    "name": "Legacy",
+    "image": "https://omeda.city/images/items/Legacy.webp"
+  },
+  {
+    "name": "Infernum",
+    "image": "https://omeda.city/images/items/Hellfire.webp"
+  },
+  {
+    "name": "Frostguard",
+    "image": "https://omeda.city/images/items/Frostguard.webp"
+  },
+  {
+    "name": "Elafrost",
+    "image": "https://omeda.city/images/items/Elafrost.webp"
+  },
+  {
+    "name": "Draconum",
+    "image": "https://omeda.city/images/items/Draconum.webp"
+  },
+  {
+    "name": "Demon Edge",
+    "image": "https://omeda.city/images/items/Demon-Edge.webp"
+  },
+  {
+    "name": "Combustion",
+    "image": "https://omeda.city/images/items/Combustion.webp"
+  },
+  {
+    "name": "Azure Core",
+    "image": "https://omeda.city/images/items/Azure-Core.webp"
+  },
+  {
+    "name": "Berserker's Axe",
+    "image": "https://omeda.city/images/items/Berserkers-Axe.webp"
+  },
+  {
+    "name": "Basilisk",
+    "image": "https://omeda.city/images/items/Basilisk.webp"
+  },
+  {
+    "name": "Rapture",
+    "image": "https://omeda.city/images/items/Rapture.webp"
+  },
+  {
+    "name": "Solaris",
+    "image": "https://omeda.city/images/items/Solaris.webp"
+  },
+  {
+    "name": "Overseer",
+    "image": "https://omeda.city/images/items/Overseer.webp"
+  },
+  {
+    "name": "Soulbinder",
+    "image": "https://omeda.city/images/items/Soulbinder.webp"
+  },
+  {
+    "name": "Envy",
+    "image": "https://omeda.city/images/items/Envy.webp"
+  },
+  {
+    "name": "Wraith Leggings",
+    "image": "https://omeda.city/images/items/Wraith-Leggings.webp"
+  },
+  {
+    "name": "Warden's Faith",
+    "image": "https://omeda.city/images/items/Wardens-Faith.webp"
+  },
+  {
+    "name": "Vanquisher",
+    "image": "https://omeda.city/images/items/Vanquisher.webp"
+  },
+  {
+    "name": "Tainted Blade",
+    "image": "https://omeda.city/images/items/Sawtoothed-Scimitar.webp"
+  },
+  {
+    "name": "Storm Breaker",
+    "image": "https://omeda.city/images/items/Storm-Breaker.webp"
+  },
+  {
+    "name": "Sky Splitter",
+    "image": "https://omeda.city/images/items/Sky-Splitter.webp"
+  },
+  {
+    "name": "Absolution",
+    "image": "https://omeda.city/images/items/Severance.webp"
+  },
+  {
+    "name": "Raiment of Renewal",
+    "image": "https://omeda.city/images/items/Raiment-of-Renewel.webp"
+  },
+  {
+    "name": "Oathkeeper",
+    "image": "https://omeda.city/images/items/Oathkeeper.webp"
+  },
+  {
+    "name": "Magnify",
+    "image": "https://omeda.city/images/items/Magnify.webp"
+  },
+  {
+    "name": "Lightning Hawk",
+    "image": "https://omeda.city/images/items/Lightning-Hawk.webp"
+  },
+  {
+    "name": "Deathstalker",
+    "image": "https://omeda.city/images/items/Fell-Blade.webp"
+  },
+  {
+    "name": "Demolisher",
+    "image": "https://omeda.city/images/items/Overlord.webp"
+  },
+  {
+    "name": "Crystalline Cuirass",
+    "image": "https://omeda.city/images/items/Crystalline_Cuirass.webp"
+  },
+  {
+    "name": "Citadel",
+    "image": "https://omeda.city/images/items/Citadel.webp"
+  },
+  {
+    "name": "Ashbringer",
+    "image": "https://omeda.city/images/items/Ashbringer.webp"
+  },
+  {
+    "name": "Aegis Of Agawar",
+    "image": "https://omeda.city/images/items/Aegis-Of-Agawar.webp"
+  },
+  {
+    "name": "Terminus",
+    "image": "https://omeda.city/images/items/Terminus.webp"
+  },
+  {
+    "name": "Megacosm",
+    "image": "https://omeda.city/images/items/Megacosm.webp"
+  },
+  {
+    "name": "Dust Devil",
+    "image": "https://omeda.city/images/items/Cyclone.webp"
+  },
+  {
+    "name": "World Breaker",
+    "image": "https://omeda.city/images/items/World-Breaker.webp"
+  },
+  {
+    "name": "The Perforator",
+    "image": "https://omeda.city/images/items/Perforator.webp"
+  },
+  {
+    "name": "Resolution",
+    "image": "https://omeda.city/images/items/Reckoning.webp"
+  },
+  {
+    "name": "Mutilator",
+    "image": "https://omeda.city/images/items/Mutilator.webp"
+  },
+  {
+    "name": "Viper",
+    "image": "https://omeda.city/images/items/Viper.webp"
+  },
+  {
+    "name": "Augmentation",
+    "image": "https://omeda.city/images/items/Augmentation.webp"
+  },
+  {
+    "name": "Imperator",
+    "image": "https://omeda.city/images/items/Imperator.webp"
+  },
+  {
+    "name": "Oblivion Crown",
+    "image": "https://omeda.city/images/items/Oblivion-Crown.webp"
+  }
+]
